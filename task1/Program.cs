5ea7a7b1894b06cab650659481da06046910fb20
LenaChen <74610301+elenachen1@users.noreply.github.com>
2022-10-21 12:11:43 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,32 @@
+﻿/* 
+Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и 
+возводит число A в натуральную степень B. Сделать в функции, сделать проверку на отрицательность.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+Console.Write("Введите положительное число:");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите положительную степень, в которую хотите возвести число:");
+int number2 = int.Parse(Console.ReadLine());
+int result = 1;
+
+if (number1 < 0 && number2 < 0)
+{
+    Console.Write("Ошибка! Введите положительные числа!");
+}
+
+else if ( number2 == 0)
+{
+    Console.Write("1");
+}
+
+else
+{
+    for (int i = 1; i <= number2; i++)
+    {
+        result *= number1;
+        //Console.Write(i);
+        //Console.Write(result);
+    }
+    Console.Write(result);
+}
