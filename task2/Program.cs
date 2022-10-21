3fa9009df91bbacaef148e5fc45cc70c85e94281
LenaChen <74610301+elenachen1@users.noreply.github.com>
2022-10-21 12:30:15 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите положительное число:");
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+
+while ( number > 0)
+{
+    int deg = number % 10;
+    sum += deg;
+    number = number / 10;
+}
+
+Console.Write(sum);
+
